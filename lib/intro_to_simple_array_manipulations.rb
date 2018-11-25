f68a8e9d87304f53915aeded9afac8c876fb2ad0
Shawanda Davis <shawandadavis93@gmail.com>
2018-11-25 20:32:10 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,13 +8,10 @@ def using_push(array,new_string)
 end 
 
 def using_unshift(array, new_string)
-  #neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  #new_neighborhood = "Brooklyn Heights"
     updated_array = array.unshift(new_string)
 end
 
 def using_pop(array)
-  #great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
     array.pop
     
     # I figured it out! I DO NOT need to define the array inside of the method... I call them outside of the method!
@@ -23,21 +20,18 @@ end
 #using_pop("")
 
 def pop_with_args(array)
-  #chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
   chars_arya_killed = array.pop(2)
   #binding.pry
 end 
 #pop_with_args("")
 
 def using_shift(array)
-  #my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = array.shift() 
+ im_so_over_this_city = array.shift() 
 
 end
 
 def shift_with_args(array)
-  #ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    brands_removed = array.shift(2)
+  brands_removed = array.shift(2)
 
 end 
 
